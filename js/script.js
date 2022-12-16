866b6a4480d7c1138cd9d684f71db44a6aa53f06
yarong88 <yarong1818@gmail.com>
2022-12-16 16:47:04 +0900

    16:46

--- js/script.js
+++ js/script.js
@@ -3,7 +3,329 @@ document.addEventListener('DOMContentLoaded', () => {
     const list_button = document.getElementById("lists-button")
     const write_button = document.getElementById("write-button")
 
-// 글작성 버튼 시작
+    function localLoad() {
+        let memory = localStorage.getItem('data_box')
+        return JSON.parse(memory) ?? []
+    }
+    // 스토리지에서 데이터 받는 함수 const 배열 = localLoad()로 받으면 됨
+    // 스토리지에 올라가 있는 data_box를 받아오는 과정
+    // 스토리지에 내용이 없다면 빈 배열로 받음
+    function localSave(data) {
+        let memory = JSON.stringify(data)
+        localStorage.setItem('data_box', memory)
+    }
+    // 데이터를 JSON화 시켜줌
+
+    const data_box = localLoad()
+    let count
+    // 고유넘버
+    if (data_box.length == 0) {
+        count = 0
+        // 스토리지에 데이터가 없으면 0(1)부터 다시 시작
+    } else {
+        count = data_box[data_box.length - 1].id_num
+        // 스토리지에서 받은 데이터(배열)의 마지막 요소의 id_num을 받아
+        // count가 겹치지 않게 
+    }
+
+    // 게시판 생성 시작
+    const lists_section = document.getElementById("lists-section")
+    const section = document.createElement('section');  //상단바메뉴를 뺀 전체 내용
+    const h2 = document.createElement('h2');    //타이틀
+    const img = document.createElement('img');  //게시판 이미지
+    const hr = document.createElement('hr');
+    const input = document.createElement('input');  //검색
+    const group = document.createElement('colgroup'); //각 컬럼에 width를 주기위한태그(colgroup 태그의 width 속성)
+    const table = document.createElement('table');  //테이블 생성
+    const thead = document.createElement('thead'); //표의 제목을 나타내는 태그
+    const tr = document.createElement('tr');
+    const tbody = document.createElement('tbody')
+    const page_numbers = document.createElement('div') // 페이지 넘버
+    const button = document.createElement('button');    //글쓰기 버튼
+
+    const width_arr = [10, 25, 10, 10, 30];     //각 컬럼에 width 부여
+    const arr = ['글번호', '제목', '작성자', '조회수', '작성일자'];     //컬럼 제목들
+
+    lists_section.appendChild(section);
+    Object.assign(section, {
+        id: 'post-list',
+        margin: '30px 15px'
+    })
+    Object.assign(h2, {
+        innerHTML: '게시판',
+        style: 'display:inline;'    //h2의 기본속성인 줄바꿈 해제
+    })
+    Object.assign(img, {
+        src: './img/post.png'
+    })
+    Object.assign(input, {
+        id: 'search_input',
+        placeholder: '검색어를 입력하세요.'
+    })
+    Object.assign(table, {
+        width: '100%'
+    })
+    Object.assign(page_numbers, {
+        id: "page-numbers"
+    })
+    Object.assign(button, {
+        id: 'btn-write',
+        textContent: '글쓰기',
+        style: 'background-color:blue;'
+    })
+    Object.assign(thead, {
+        id: "thead-01"
+    })
+    Object.assign(tbody, {
+        id: "tbody-01"
+    })
+    section.append(img, h2, hr, input, table, page_numbers, button);
+    table.append(group, thead, tbody);
+    thead.appendChild(tr);
+
+    width_arr.forEach(v => {
+        const col = document.createElement('col');
+        col.style.cssText = `width : ${v}%`;
+        group.appendChild(col);
+    })
+    arr.forEach(v => {
+        const th = document.createElement('th');
+        th.textContent = v;
+        tr.appendChild(th);
+    })
+
+    // 리스트 작성 함수 시작
+    const list_cteate = function (a) {
+        const tbody = document.getElementById("tbody-01")
+        const tr1 = document.createElement("tr");
+        const numtd = document.createElement('td');         //글번호 td
+        const titletd = document.createElement('td');       //제목 td
+        const writertd = document.createElement('td');      //작성자 td
+        const viewstd = document.createElement('td');       //조회수 td
+        const datatd = document.createElement('td');        //작성일자 td
+
+        numtd.textContent = data_box[a].id_num;
+        titletd.textContent = data_box[a].title;
+        writertd.textContent = data_box[a].views;
+        viewstd.textContent = data_box[a].writer;
+        datatd.textContent = data_box[a].date;
+
+        tbody.appendChild(tr1);
+        tr1.append(numtd, titletd, writertd, viewstd, datatd);
+
+        tr1.addEventListener('click', (event) => {
+            list_revise()
+        })
+    }
+    // 리스트 작성 함수 끝
+
+
+    const list_revise = function () {
+
+        const div0 = document.getElementById("confirm-section")
+        div0.setAttribute('class', 'dd')
+        //클릭후 div화면 on
+        let arr = [];
+        let count = 0;
+        let views = 0;
+        let now = 0;
+
+
+        //타이틀 글생성
+        const div00 = document.createElement('div')//버튼후 젤큰div
+        const div1 = document.createElement('div') //글번호
+        const div2 = document.createElement('div') //제목
+        const div3 = document.createElement('div') //조회수
+        const div4 = document.createElement('div') //작성자
+        const div5 = document.createElement('div') //작성일시
+        const div6 = document.createElement('div') //내용
+        const div7 = document.createElement('div') //첨부이미지
+        const img = document.createElement('img') //이미지내용
+        //내용 생성
+        const text1 = document.createElement('textarea')
+        const text2 = document.createElement('textarea')
+        const text3 = document.createElement('textarea')
+        const text4 = document.createElement('textarea')
+        const text5 = document.createElement('textarea')
+        const text6 = document.createElement('textarea')
+
+        //버튼생성
+        const button1 = document.createElement('button')
+        const button2 = document.createElement('button')
+        const button3 = document.createElement('button')
+        const button4 = document.createElement('button')
+        const button5 = document.createElement('button')
+        //글머리
+        const span1 = document.createElement('span')
+        const span2 = document.createElement('span')
+        const span3 = document.createElement('span')
+        const span4 = document.createElement('span')
+        const span5 = document.createElement('span')
+        const span6 = document.createElement('span')
+        const span7 = document.createElement('span')
+
+        //span class
+        span1.setAttribute('class', 'span_op')
+        span2.setAttribute('class', 'span_op')
+        span3.setAttribute('class', 'span_op')
+        span4.setAttribute('class', 'span_op')
+        span5.setAttribute('class', 'span_op')
+        span6.setAttribute('class', 'span_op')
+
+        //div 타이틀
+        span1.textContent = '글번호'
+        span2.textContent = '제목'
+        span3.textContent = '조회수'
+        span4.textContent = '작성자'
+        span5.textContent = '작성일시'
+        span6.textContent = '내용'
+        span7.textContent = '첨부이미지'
+        //div class
+        div00.setAttribute('class', 'div_op0')
+        div1.setAttribute('class', 'div_op')
+        div2.setAttribute('class', 'div_op')
+        div3.setAttribute('class', 'div_op')
+        div4.setAttribute('class', 'div_op')
+        div5.setAttribute('class', 'div_op')
+        div6.setAttribute('class', 'div_op')
+        div7.setAttribute('class', 'div_op')
+
+        //img 넣기
+        // img.setAttribute('src', '/goodimg/코딩싫어.jpg')
+        img.setAttribute('class', 'sp')
+
+        //textarea Css
+        text1.style.cssText = 'border : 1px soild #000;'
+        text2.style.cssText = 'border : 1px soild #000;'
+        text3.style.cssText = 'border : 1px soild #000;'
+        text4.style.cssText = 'border : 1px soild #000;'
+        text5.style.cssText = 'border : 1px soild #000;'
+        text6.style.cssText = 'border : 1px soild #000;'
+
+        //버튼 타이틀
+        button1.textContent = '목록'
+        button2.textContent = '저장'
+        button3.textContent = '삭제'
+        button4.textContent = '이전글'
+        button5.textContent = '다음글'
+
+        //버튼 class
+        button1.setAttribute('class', 'btn_list')
+        button2.setAttribute('class', 'btn_co')
+        button3.setAttribute('class', 'btn_del')
+        button4.setAttribute('class', 'btn_before')
+        button5.setAttribute('class', 'btn_after')
+
+        // 출력
+        div0.appendChild(div00).append(div1, div2, div3, div4, div5, div6, div7,
+            button1, button2, button3, button4, button5)
+        div1.append(span1, text1)
+        div2.append(span2, text2)
+        div3.append(span3, text3)
+        div4.append(span4, text4)
+        div5.append(span5, text5)
+        div6.append(span6, text6)
+        div7.append(span7, img)
+
+        //오브젝트
+        const obj = {
+            id: count,
+            text: text2.value,
+            views: 0,
+            writer: text4.value,
+            date: now,
+            content: text6.value
+        }
+
+        //local값
+        Object.assign(text1, {
+
+        })
+        Object.assign(text2, {
+
+        })
+        Object.assign(text3, {
+
+        })
+        Object.assign(text4, {
+
+        })
+        Object.assign(text5, {
+
+        })
+        Object.assign(text6, {
+
+        })
+
+
+        // 삭제버튼
+        function del() {
+            div0.removeChild(div00)
+        }
+        button3.addEventListener('click', del)
+        // 저장버튼
+        let state = false
+        function cor() {
+            // div1.removeChild(input1)
+            if (!state) {
+                text1.style.cssText = 'border : 0;'
+                text2.style.cssText = 'border : 0;'
+                text3.style.cssText = 'border : 0;'
+                text4.style.cssText = 'border : 0;'
+                text5.style.cssText = 'border : 0;'
+                text6.style.cssText = 'border : 0;'
+                arr.push(obj)
+                localStorage.setItem('good', JSON.stringify(arr))
+                console.log(arr)
+            }
+            // else{
+            //     input1.style.cssText = 'border : 0;'
+            //     input2.style.cssText = 'border : 0;'
+            //     input3.style.cssText = 'border : 0;'
+            //     input4.style.cssText = 'border : 0;'
+            //     input5.style.cssText = 'border : 0;'
+            //     input6.style.cssText = 'border : 0;'}
+            // state = !state
+        }
+        button2.addEventListener('click', cor)
+    }
+
+
+
+
+
+
+    for (let i = data_box.length - 1; i > data_box.length - 11; i--) {
+        list_cteate(i)
+    }
+
+    const page_num = Math.ceil(data_box.length / 10)
+    if (data_box.length > 0) {
+        for (let j = page_num; j > 0; j--) {
+            const page_number = document.createElement('span')
+            page_numbers.appendChild(page_number)
+            page_number.textContent = j
+            page_number.addEventListener('click', (event) => {
+                const tbody_01 = document.getElementById("tbody-01")
+                table.removeChild(tbody_01)
+                const tbody = document.createElement('tbody')
+                tbody.setAttribute("id", "tbody-01")
+                table.appendChild(tbody)
+                if (j !== 1) {
+                    for (let k = data_box.length - 1 - (page_num - j) * 10; k > data_box.length - 11 - (page_num - j) * 10; k--) {
+                        list_cteate(k)
+                    }
+                } else {
+                    for (let k = data_box.length - 1 - (page_num - j) * 10; k >= 0; k--) {
+                        list_cteate(k)
+                    }
+                }
+            })
+        }
+    }
+    //게시판 작성 끝
+
+    // 글작성 버튼 시작
     write_button.addEventListener('click', (event) => {
         // 글작성 창이 하나만 뜨게
         if (!document.getElementById('write-write-con')) {
@@ -147,19 +469,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 textContent: "목록"
             })
 
-            function localLoad() {
-                let memory = localStorage.getItem('data_box')
-                return JSON.parse(memory) ?? []
-            }
-            // 스토리지에서 데이터 받는 함수 const 배열 = localLoad()로 받으면 됨
-            // 스토리지에 올라가 있는 data_box를 받아오는 과정
-            // 스토리지에 내용이 없다면 빈 배열로 받음
-            function localSave(data) {
-                let memory = JSON.stringify(data)
-                localStorage.setItem('data_box', memory)
-            }
-            // 데이터를 JSON화 시켜줌
-
             const data_box = localLoad()
             let count
             // 고유넘버
@@ -202,27 +511,6 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
-// 글작성 버튼 끝
+    // 글작성 버튼 끝
 
-    // // ex) data.length가 45일때
-    // for (let i = 0; i < data.length / 10; i++) {
-    //     // 어차피 <=가 아닌 < 이거 니까 나누기 10 했을때 소수점을 생각할 필요가 없다.
-    //     const list_num = document.createElement('span')
-    //     lists_footer.appendChild(list_num)
-    //     // 하단에 숫자 부여
-    //     list_num.textContent = `${data.length / 10 - data.length % 10 + 1 - i}`
-    //     // 보통 역순으로 표시되니까 ex) 5 4 3 2 1
-    //     list_num.addEventListener('click', (event) => {
-    //         lists_div.removeChild(lists_middle)
-    //         // 작성하기 전에 전 게시글들을 지우고
-    //         for (let j = data.length - 1; j > data.length - 1 - (10 * (i + 1)); j--) {
-    //             data[j] //data[j]에서 뽑아낸 데이타로 한 줄 작성
-    //             // 이걸 10번 반복 ex) 45~36까지
-    //         }
-    //     })
-    //     // 5번이라는 span에 게시글 45~36번까지 게시글을 10개 생성하는 이벤트가 부여
-    //     // 5라고 적혀있지만 첫번째 버튼, 다섯번째 버튼까지 작성
-    // }
-
-  
 })
